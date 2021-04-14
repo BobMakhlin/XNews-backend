3d05c92b47db61478a5e02d70133ee26f9624539
Bob Makhlin <borismakhlin5@gmail.com>
2021-04-14 21:23:04 +0300

    Add base type for all controllers

---
+++ Presentation.API/Controllers/MyBaseController.cs
@@ -0,0 +1,25 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Presentation.Controllers
+{
+    /// <summary>
+    /// Represents a base type for all controllers.
+    /// </summary>
+    public class MyBaseController : ControllerBase
+    {
+        #region Fields
+
+        private IMediator _mediator;
+
+        #endregion
+
+        #region Properties
+
+        protected IMediator Mediator =>
+            _mediator ??= HttpContext.RequestServices.GetService<IMediator>();
+
+        #endregion
+    }
+}
--- Presentation.API/Controllers/PostsController.cs
+++ Presentation.API/Controllers/PostsController.cs
@@ -1,10 +1,12 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Presentation.Controllers;
 
 namespace Presentation.API.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class PostsController : ControllerBase
+    public class PostsController : MyBaseController
     {
         
     }

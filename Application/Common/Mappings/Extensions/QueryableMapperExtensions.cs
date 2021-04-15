c9891d81d8844bc5ebf6e004c3a07c53114f0df4
Bob Makhlin <borismakhlin5@gmail.com>
2021-04-15 10:43:11 +0300

    Add extension methods for IQueryable to work easier with AutoMapper

---
+++ Application/Common/Mappings/Extensions/QueryableMapperExtensions.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using AutoMapper.QueryableExtensions;
+using Microsoft.EntityFrameworkCore;
+using IConfigurationProvider = AutoMapper.IConfigurationProvider;
+
+namespace Application.Common.Mappings.Extensions
+{
+    /// <summary>
+    /// Contains extension-methods for type <see cref="IQueryable"/> that allow to work easier with AutoMapper.
+    /// </summary>
+    public static class QueryableMapperExtensions
+    {
+        /// <summary>
+        /// Converts the given <paramref name="query"/> to type <see cref="List{TDestination}"/>.
+        /// </summary>
+        /// <param name="query"></param>
+        /// <param name="provider">Mapper configuration</param>
+        /// <param name="cancellationToken"></param>
+        /// <typeparam name="TDestination"></typeparam>
+        /// <returns></returns>
+        public static async Task<List<TDestination>> ProjectToListAsync<TDestination>(this IQueryable query,
+            IConfigurationProvider provider, CancellationToken cancellationToken = default)
+        {
+            return await query
+                .ProjectTo<TDestination>(provider)
+                .ToListAsync(cancellationToken);
+        }
+
+        /// <summary>
+        /// Converts the given <paramref name="query"/> to type <see cref="IQueryable{TDestination}"/>
+        /// and calls <see cref="o:EntityFrameworkQueryableExtensions.SingleOrDefaultAsync"/> on it.
+        /// </summary>
+        /// <param name="query"></param>
+        /// <param name="provider"></param>
+        /// <param name="cancellationToken"></param>
+        /// <typeparam name="TDestination"></typeparam>
+        /// <returns></returns>
+        public static async Task<TDestination> ProjectToSingleOrDefaultAsync<TDestination>(this IQueryable query,
+            IConfigurationProvider provider, CancellationToken cancellationToken = default)
+        {
+            return await query
+                .ProjectTo<TDestination>(provider)
+                .SingleOrDefaultAsync(cancellationToken);
+        }
+    }
+}

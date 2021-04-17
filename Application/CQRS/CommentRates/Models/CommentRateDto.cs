b5fd43dfa3b7532376f02bd5f93768dd83292060
Bob Makhlin <borismakhlin5@gmail.com>
2021-04-17 20:12:05 +0300

    Add the ability to get rates of a comment
    CommentsController now handles the following request: GET /comments/{id}/rates

---
+++ Application/CQRS/CommentRates/Models/CommentRateDto.cs
@@ -0,0 +1,17 @@
+﻿
+using System;
+using Application.Common.Mappings.Interfaces;
+using Domain.Entities;
+
+namespace Application.CQRS.CommentRates.Models
+{
+    public class CommentRateDto : IMapFrom<CommentRate>
+    {
+        #region Properties
+
+        public Guid CommentRateId { get; set; }
+        public double Rate { get; set; }
+
+        #endregion
+    }
+}
---
+++ Presentation.API/Controllers/CommentsController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Application.CQRS.CommentRates.Models;
+using Application.CQRS.Comments.Queries;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Presentation.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CommentsController : MyBaseController
+    {
+        [HttpGet("{id}/rates")]
+        public async Task<IActionResult> GetRatesOfCommentAsync(Guid id)
+        {
+            IEnumerable<CommentRateDto> rates = await Mediator.Send(new GetRatesOfCommentQuery {CommentId = id});
+            return Ok(rates);
+        }
+    }
+}

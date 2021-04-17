f55dd50ec047708a22a400e1196496de01681e17
Bob Makhlin <borismakhlin5@gmail.com>
2021-04-17 22:27:22 +0300

    Add the ability to create new comment
    CommentsController now handles POST-requests.

---
+++ Application/CQRS/Comments/Commands/CreateCommentCommand.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Application.Persistence.Interfaces;
+using Domain.Entities;
+using MediatR;
+
+namespace Application.CQRS.Comments.Commands
+{
+    public class CreateCommentCommand : IRequest<Guid>
+    {
+        public string Content { get; set; }
+        public Guid PostId { get; set; }
+        public Guid? ParentCommentId { get; set; }
+
+        public class Handler : IRequestHandler<CreateCommentCommand, Guid>
+        {
+            #region Fields
+
+            private readonly IXNewsDbContext _context;
+
+            #endregion
+            
+            #region Constructors
+
+            public Handler(IXNewsDbContext context)
+            {
+                _context = context;
+            }
+
+            #endregion
+            
+            #region IRequestHandler<CreateCommentCommand, Guid>
+
+            public async Task<Guid> Handle(CreateCommentCommand request, CancellationToken cancellationToken)
+            {
+                Comment comment = ConvertToComment(request);
+
+                await CreateCommentAsync(comment, cancellationToken)
+                    .ConfigureAwait(false);
+
+                return comment.CommentId;
+            }
+
+            #endregion
+
+            #region Methods
+
+            /// <summary>
+            /// Creates an object of type <see cref="Comment"/> based on the given <paramref name="command"/>.
+            /// </summary>
+            /// <param name="command"></param>
+            /// <returns>The created object of type <see cref="Comment"/></returns>
+            private Comment ConvertToComment(CreateCommentCommand command)
+            {
+                return new()
+                {
+                    Content = command.Content,
+                    PostId = command.PostId,
+                    ParentCommentId = command.ParentCommentId
+                };
+            }
+
+            private async Task CreateCommentAsync(Comment comment, CancellationToken cancellationToken)
+            {
+                await _context.Comment.AddAsync(comment, cancellationToken)
+                    .ConfigureAwait(false);
+
+                await _context.SaveChangesAsync(cancellationToken)
+                    .ConfigureAwait(false);
+            }
+
+            #endregion
+        }
+    }
+}
--- Presentation.API/Controllers/CommentsController.cs
+++ Presentation.API/Controllers/CommentsController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Application.CQRS.CommentRates.Models;
+using Application.CQRS.Comments.Commands;
 using Application.CQRS.Comments.Queries;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,6 +12,13 @@ namespace Presentation.API.Controllers
     [Route("[controller]")]
     public class CommentsController : MyBaseController
     {
+        [HttpPost]
+        public async Task<IActionResult> CreateCommentAsync(CreateCommentCommand request)
+        {
+            Guid createdCommentId = await Mediator.Send(request);
+            return Ok(createdCommentId);
+        }
+        
         [HttpGet("{id}/rates")]
         public async Task<IActionResult> GetRatesOfCommentAsync(Guid id)
         {

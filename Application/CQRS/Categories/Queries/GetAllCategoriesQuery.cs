97e48d3311445fa2884ca127e90a37bc61902a92
Bob Makhlin <borismakhlin5@gmail.com>
2021-04-17 10:20:53 +0300

    Add the ability to get all categories
    CategoriesController now handles GET-requests.

---
+++ Application/CQRS/Categories/Queries/GetAllCategoriesQuery.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Application.Common.Extensions;
+using Application.CQRS.Categories.Models;
+using Application.Persistence.Interfaces;
+using AutoMapper;
+using MediatR;
+
+namespace Application.CQRS.Categories.Queries
+{
+    public class GetAllCategoriesQuery : IRequest<IEnumerable<CategoryDto>>
+    {
+        public class Handler : IRequestHandler<GetAllCategoriesQuery, IEnumerable<CategoryDto>>
+        {
+            #region Fields
+
+            private IXNewsDbContext _context;
+            private IMapper _mapper;
+
+            #endregion
+
+            #region Constructors
+
+            public Handler(IXNewsDbContext context, IMapper mapper)
+            {
+                _context = context;
+                _mapper = mapper;
+            }
+
+            #endregion
+
+            #region IRequestHandler<GetAllCategoriesQuery, IEnumerable<CategoryDto>>
+
+            public async Task<IEnumerable<CategoryDto>> Handle(GetAllCategoriesQuery request,
+                CancellationToken cancellationToken)
+            {
+                return await _context.Category
+                    .ProjectToListAsync<CategoryDto>(_mapper.ConfigurationProvider, cancellationToken)
+                    .ConfigureAwait(false);
+            }
+
+            #endregion
+        }
+    }
+}
---
+++ Presentation.API/Controllers/CategoriesController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Application.CQRS.Categories.Models;
+using Application.CQRS.Categories.Queries;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Presentation.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CategoriesController : MyBaseController
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetAllCategoriesAsync()
+        {
+            IEnumerable<CategoryDto> categories = await Mediator.Send(new GetAllCategoriesQuery());
+            return Ok(categories);
+        }
+    }
+}

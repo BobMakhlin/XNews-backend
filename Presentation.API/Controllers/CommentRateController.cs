cc478b074daca99ac9697c60b95f309d69e37112
Bob Makhlin <borismakhlin5@gmail.com>
2021-04-17 20:32:05 +0300

    Add the ability to add a rate to a comment
    CommentRatesController now handles POST-requests.

---
+++ Presentation.API/Controllers/CommentRateController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+using Application.CQRS.CommentRates.Commands;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Presentation.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CommentRateController : MyBaseController
+    {
+        [HttpPost]
+        public async Task<IActionResult> CreateCommentRate(CreateCommentRateCommand request)
+        {
+            Guid createdCommentRateId = await Mediator.Send(request);
+            return Ok(createdCommentRateId);
+        }
+    }
+}

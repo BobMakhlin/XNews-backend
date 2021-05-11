6a34fb2c18a748328ff9e2259485e5c196f74d2c
Bob Makhlin <borismakhlin5@gmail.com>
2021-05-11 19:25:43 +0300

    Add migration, creating fn_PostCommentHierarchy SQL-function
    This migration is related to primary database.

---
+++ Persistence.Primary/Migrations/20210511155910_AddPostCommentHierarchyFunction.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Persistence.Primary.Migrations
+{
+    public partial class AddPostCommentHierarchyFunction : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql
+            (@"
+				CREATE FUNCTION [dbo].[fn_PostCommentHierarchy] (@postId UNIQUEIDENTIFIER, @pageNumber INT, @pageSize INT)
+				RETURNS TABLE
+				AS
+				RETURN
+				(
+					WITH cte AS
+					(
+						SELECT CommentId, Content, PostId, ParentCommentId
+						FROM dbo.Comment
+						WHERE ParentCommentId IS NULL and PostId = @postId
+						ORDER BY PostId
+						OFFSET (@pageNumber - 1) * @pageSize ROWS FETCH NEXT @pageSize ROWS ONLY
+
+						UNION ALL
+
+						SELECT child.CommentId, child.Content, child.PostId, child.ParentCommentId
+						FROM dbo.Comment child
+						INNER JOIN cte parent
+						ON parent.CommentId = child.ParentCommentId
+						WHERE parent.PostId = @postId
+					)
+
+					SELECT CommentId, Content, PostId, ParentCommentId FROM cte
+				);
+			");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+	        migrationBuilder.Sql("DROP FUNCTION [dbo].[fn_PostCommentHierarchy]");
+        }
+    }
+}

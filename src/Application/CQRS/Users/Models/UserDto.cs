b78963e1c9540b36736fb82f246784295a9145d2
Bob Makhlin <borismakhlin5@gmail.com>
2021-06-06 16:28:18 +0300

    Rename property 'Id' into 'UserId' inside of UserDto

--- src/Application/CQRS/Users/Models/UserDto.cs
+++ src/Application/CQRS/Users/Models/UserDto.cs
@@ -1,13 +1,33 @@
 using Application.Common.Mappings.Interfaces;
 using Application.Identity.Models;
+using AutoMapper;
 
 namespace Application.CQRS.Users.Models
 {
     public class UserDto : IMapFrom<ApplicationUser>
     {
-        public string Id { get; set; }
+        #region Properties
+
+        public string UserId { get; set; }
         public string UserName { get; set; }
         public string Email { get; set; }
         public string PhoneNumber { get; set; }
+
+        #endregion
+
+        #region IMapFrom<ApplicationUser>
+
+        public void MapUsingProfile(Profile profile)
+        {
+            profile.CreateMap<ApplicationUser, UserDto>()
+                .ForMember
+                (
+                    x => x.UserId,
+                    opts =>
+                        opts.MapFrom(x => x.Id)
+                );
+        }
+
+        #endregion
     }
 }

cc9f4c6f7ffae88e689e452be5149b2f42826a58
Bob Makhlin <borismakhlin5@gmail.com>
2021-06-06 16:56:51 +0300

    Rename property 'Id' into 'RoleId' inside of RoleDto

--- src/Application/CQRS/Roles/Models/RoleDto.cs
+++ src/Application/CQRS/Roles/Models/RoleDto.cs
@@ -1,11 +1,32 @@
+using System;
 using Application.Common.Mappings.Interfaces;
 using Application.Identity.Models;
+using AutoMapper;
 
 namespace Application.CQRS.Roles.Models
 {
     public class RoleDto : IMapFrom<ApplicationRole>
     {
-        public string Id { get; set; }
+        #region Properties
+
+        public string RoleId { get; set; }
         public string Name { get; set; }
+
+        #endregion
+
+        #region IMapFrom<ApplicationRole>
+
+        public void MapUsingProfile(Profile profile)
+        {
+            profile.CreateMap<ApplicationRole, RoleDto>()
+                .ForMember
+                (
+                    x => x.RoleId,
+                    opts =>
+                        opts.MapFrom(x => x.Id)
+                );
+        }
+
+        #endregion
     }
 }

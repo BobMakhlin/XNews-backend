1e542e88de6ab4869fae5981d5529434c403bfe1
Bob Makhlin <borismakhlin5@gmail.com>
2021-06-07 17:28:37 +0300

    Throw NotFoundException in GetPostRatesOfUserQuery if item not found
    When a user doesn't exist, we throw a NotFoundException.

--- src/Application/CQRS/Users/Queries/GetPostRatesOfUserQuery.cs
+++ src/Application/CQRS/Users/Queries/GetPostRatesOfUserQuery.cs
@@ -1,7 +1,11 @@
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using Application.Common.Exceptions;
 using Application.CQRS.PostRates.Models;
+using Application.Identity.Extensions;
+using Application.Identity.Interfaces;
+using Application.Identity.Models;
 using Application.Pagination.Common.Models;
 using Application.Pagination.Common.Models.PagedList;
 using Application.Pagination.Extensions;
@@ -35,15 +39,18 @@ namespace Application.CQRS.Users.Queries
 
             private readonly IMapper _mapper;
             private readonly IXNewsDbContext _context;
+            private readonly IIdentityStorage<ApplicationUser, string> _userStorage;
 
             #endregion
 
             #region Constructors
 
-            public Handler(IXNewsDbContext context, IMapper mapper)
+            public Handler(IXNewsDbContext context, IMapper mapper,
+                IIdentityStorage<ApplicationUser, string> userStorage)
             {
                 _context = context;
                 _mapper = mapper;
+                _userStorage = userStorage;
             }
 
             #endregion
@@ -53,12 +60,23 @@ namespace Application.CQRS.Users.Queries
             public async Task<IPagedList<PostRateDto>> Handle(GetPostRatesOfUserQuery request,
                 CancellationToken cancellationToken)
             {
-                return await _context.PostRate
+                IPagedList<PostRateDto> postRates = await _context.PostRate
                     .Where(pr => pr.UserId == request.UserId)
                     .OrderBy(pr => pr.PostRateId)
                     .ProjectTo<PostRateDto>(_mapper.ConfigurationProvider)
                     .ProjectToPagedListAsync(request, cancellationToken)
                     .ConfigureAwait(false);
+
+                if (postRates.TotalItemsCount > 0)
+                {
+                    return postRates;
+                }
+
+                bool userExists = await _userStorage.Exists(request.UserId)
+                    .ConfigureAwait(false);
+                return userExists
+                    ? PagedList<PostRateDto>.CreateEmptyPagedList(request)
+                    : throw new NotFoundException();
             }
 
             #endregion

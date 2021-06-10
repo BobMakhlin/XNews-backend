c1b8ea85aaaebcdbd53cdb28634d9f358bf584cc
Bob Makhlin <borismakhlin5@gmail.com>
2021-06-10 15:37:28 +0300

    Add extension-method ThrowIfDoesNotExistAsync to IIdentityStorage, DbSet

---
+++ src/Application/Common/Extensions/DbSetExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading.Tasks;
+using Application.Common.Exceptions;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Common.Extensions
+{
+    /// <summary>
+    /// Contains extension-methods for type <see cref="DbSet{TEntity}"/>.
+    /// </summary>
+    public static class DbSetExtensions
+    {
+        /// <summary>
+        /// Throws an exception of type <see cref="NotFoundException"/> if the item
+        /// with the specified <paramref name="primaryKeyValues"/> doesn't exist.
+        /// </summary>
+        public static async Task ThrowIfDoesNotExistAsync<TEntity>(this DbSet<TEntity> dbSet,
+            params object[] primaryKeyValues)
+            where TEntity : class
+        {
+            TEntity entity = await dbSet.FindAsync(primaryKeyValues)
+                .ConfigureAwait(false);
+            
+            if (entity == null)
+            {
+                throw new NotFoundException();
+            }
+        }
+    }
+}
---
+++ src/Application/Common/Extensions/IIdentityStorageExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading.Tasks;
+using Application.Common.Exceptions;
+using Application.Identity.Extensions;
+using Application.Identity.Interfaces;
+
+namespace Application.Common.Extensions
+{
+    /// <summary>
+    /// Contains extension-methods for type <see cref="IIdentityStorage{TItem,TItemId}"/>.
+    /// </summary>
+    public static class IIdentityStorageExtensions
+    {
+        /// <summary>
+        /// Throws <see cref="NotFoundException"/> if the item with the specified <paramref name="id"/> doesn't exist.
+        /// </summary>
+        public static async Task ThrowIfDoesNotExistAsync<TItem, TItemId>(this IIdentityStorage<TItem, TItemId> storage,
+            TItemId id)
+        {
+            bool itemExists = await storage.Exists(id)
+                .ConfigureAwait(false);
+
+            if (!itemExists)
+            {
+                throw new NotFoundException();
+            }
+        }
+    }
+}

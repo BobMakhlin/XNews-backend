85f7c1c1e0636d40c77a1c2d5d06847ed01a249f
Bob Makhlin <borismakhlin5@gmail.com>
2021-06-11 17:11:56 +0300

    Don't allow the user to rate the same comment twice via FluentValidation

--- src/Application.Validation/AbstractValidators/CQRS/Comments/Commands/CommentRate/AddRateToCommentCommandValidator.cs
+++ src/Application.Validation/AbstractValidators/CQRS/Comments/Commands/CommentRate/AddRateToCommentCommandValidator.cs
@@ -1,22 +1,53 @@
+using System.Threading;
+using System.Threading.Tasks;
 using Application.CQRS.Comments.Commands.CommentRate;
+using Application.Persistence.Interfaces;
 using Application.Validation.Options;
 using Application.Validation.Tools.Extensions;
 using FluentValidation;
+using Microsoft.EntityFrameworkCore;
 
 namespace Application.Validation.AbstractValidators.CQRS.Comments.Commands.CommentRate
 {
     public class AddRateToCommentCommandValidator : AbstractValidator<AddRateToCommentCommand>
     {
-        public AddRateToCommentCommandValidator()
+        #region Fields
+
+        private readonly IXNewsDbContext _context;
+
+        #endregion
+
+        #region Constructors
+
+        public AddRateToCommentCommandValidator(IXNewsDbContext context)
         {
+            _context = context;
+
             RuleFor(c => c.CommentId)
                 .NotEmpty();
-            
+
             RuleFor(c => c.Rate)
                 .In(CommentRateValidationOptions.NegativeRate, CommentRateValidationOptions.PositiveRate);
 
             RuleFor(c => c.UserId)
-                .NotEmpty();
+                .NotEmpty()
+                .MustAsync(NotHaveRatedTheCurrentComment).WithMessage("User has already rated this comment");
         }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Checks if the user with the specified <paramref name="userId"/> has already rated the comment
+        /// with the specified <paramref name="command.CommentId"/>.
+        /// </summary>
+        private async Task<bool> NotHaveRatedTheCurrentComment(AddRateToCommentCommand command, string userId, CancellationToken token)
+        {
+            return !await _context.CommentRate.AnyAsync(commentRate =>
+                commentRate.UserId == userId && commentRate.CommentId == command.CommentId, token);
+        }
+
+        #endregion
     }
 }

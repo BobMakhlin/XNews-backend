7d76e5fab4928b24c3a9367cf6068b1216753009
Bob Makhlin <borismakhlin5@gmail.com>
2021-06-11 17:29:10 +0300

    Don't allow the user to rate the same post twice via FluentValidation

--- src/Application.Validation/AbstractValidators/CQRS/Posts/Commands/PostRate/AddRateToPostCommandValidator.cs
+++ src/Application.Validation/AbstractValidators/CQRS/Posts/Commands/PostRate/AddRateToPostCommandValidator.cs
@@ -1,22 +1,55 @@
+using System.Threading;
+using System.Threading.Tasks;
 using Application.CQRS.Posts.Commands.PostRate;
+using Application.Persistence.Interfaces;
 using Application.Validation.Options;
 using Application.Validation.Tools.Extensions;
 using FluentValidation;
+using Microsoft.EntityFrameworkCore;
 
 namespace Application.Validation.AbstractValidators.CQRS.Posts.Commands.PostRate
 {
     public class AddRateToPostCommandValidator : AbstractValidator<AddRateToPostCommand>
     {
-        public AddRateToPostCommandValidator()
+        #region Fields
+
+        private readonly IXNewsDbContext _context;
+
+        #endregion
+
+        #region Constructors
+
+        public AddRateToPostCommandValidator(IXNewsDbContext context)
         {
+            _context = context;
+
             RuleFor(c => c.PostId)
                 .NotEmpty();
-            
+
             RuleFor(c => c.Rate)
                 .In(PostRateValidationOptions.NegativeRate, PostRateValidationOptions.PositiveRate);
 
             RuleFor(c => c.UserId)
-                .NotEmpty();
+                .NotEmpty()
+                .MustAsync(NotHaveRatedTheCurrentPost).WithMessage("User has already rated this post");
         }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Checks if the user with the specified <paramref name="userId"/> has already rated the post
+        /// with the specified <paramref name="command.PostId"/>.
+        /// </summary>
+        private async Task<bool> NotHaveRatedTheCurrentPost(AddRateToPostCommand command, string userId,
+            CancellationToken token)
+        {
+            return !await _context.PostRate.AnyAsync(postRate =>
+                    postRate.UserId == userId && postRate.PostId == command.PostId, token)
+                .ConfigureAwait(false);
+        }
+
+        #endregion
     }
 }

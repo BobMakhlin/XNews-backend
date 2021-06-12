7fd514f502edbe241b9682b94638b23752115216
Bob Makhlin <borismakhlin5@gmail.com>
2021-06-12 19:14:11 +0300

    Refactor AddRateToPostCommandValidator
    We used UniqueForColumnsCombinationInDbSet validator inside, to prevent the user from rating the same post more than once.

--- src/Application.Validation/AbstractValidators/CQRS/Posts/Commands/PostRate/AddRateToPostCommandValidator.cs
+++ src/Application.Validation/AbstractValidators/CQRS/Posts/Commands/PostRate/AddRateToPostCommandValidator.cs
@@ -1,28 +1,15 @@
-using System.Threading;
-using System.Threading.Tasks;
 using Application.CQRS.Posts.Commands.PostRate;
 using Application.Persistence.Interfaces;
 using Application.Validation.Options;
 using Application.Validation.Tools.Extensions;
 using FluentValidation;
-using Microsoft.EntityFrameworkCore;
 
 namespace Application.Validation.AbstractValidators.CQRS.Posts.Commands.PostRate
 {
     public class AddRateToPostCommandValidator : AbstractValidator<AddRateToPostCommand>
     {
-        #region Fields
-
-        private readonly IXNewsDbContext _context;
-
-        #endregion
-
-        #region Constructors
-
         public AddRateToPostCommandValidator(IXNewsDbContext context)
         {
-            _context = context;
-
             RuleFor(c => c.PostId)
                 .NotEmpty();
 
@@ -30,26 +17,12 @@ namespace Application.Validation.AbstractValidators.CQRS.Posts.Commands.PostRate
                 .In(PostRateValidationOptions.NegativeRate, PostRateValidationOptions.PositiveRate);
 
             RuleFor(c => c.UserId)
-                .NotEmpty()
-                .MustAsync(NotHaveRatedTheCurrentPost).WithMessage("User has already rated this post");
+                .NotEmpty();
+
+            RuleFor(c => new object[] {c.UserId, c.PostId})
+                .UniqueForColumnsCombinationInDbSet(context.PostRate,
+                    postRate => new {postRate.UserId, postRate.PostId})
+                .WithMessage("User has already rated this post");
         }
-
-        #endregion
-
-        #region Methods
-
-        /// <summary>
-        /// Checks if the user with the specified <paramref name="userId"/> has already rated the post
-        /// with the specified <paramref name="command.PostId"/>.
-        /// </summary>
-        private async Task<bool> NotHaveRatedTheCurrentPost(AddRateToPostCommand command, string userId,
-            CancellationToken token)
-        {
-            return !await _context.PostRate.AnyAsync(postRate =>
-                    postRate.UserId == userId && postRate.PostId == command.PostId, token)
-                .ConfigureAwait(false);
-        }
-
-        #endregion
     }
 }

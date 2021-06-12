eac227553803ee0fe2f5f126a92f73c29ebe008d
Bob Makhlin <borismakhlin5@gmail.com>
2021-06-12 21:54:11 +0300

    Fix #19

--- src/Application.Validation/AbstractValidators/CQRS/Categories/Commands/CategoryStorage/UpdateCategoryCommandValidator.cs
+++ src/Application.Validation/AbstractValidators/CQRS/Categories/Commands/CategoryStorage/UpdateCategoryCommandValidator.cs
@@ -1,22 +1,60 @@
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 using Application.CQRS.Categories.Commands.CategoryStorage;
 using Application.Persistence.Interfaces;
 using Application.Validation.Options;
 using Application.Validation.Tools.Extensions;
 using FluentValidation;
+using Microsoft.EntityFrameworkCore;
 
 namespace Application.Validation.AbstractValidators.CQRS.Categories.Commands.CategoryStorage
 {
     public class UpdateCategoryCommandValidator : AbstractValidator<UpdateCategoryCommand>
     {
+        #region Fields
+
+        private readonly IXNewsDbContext _context;
+
+        #endregion
+
+        #region Constructors
+
         public UpdateCategoryCommandValidator(IXNewsDbContext context)
         {
+            _context = context;
+            
             RuleFor(c => c.CategoryId)
                 .NotEmpty();
 
             RuleFor(c => c.Title)
                 .NotEmpty()
                 .Length(CategoryValidationOptions.TitleMinLength, CategoryValidationOptions.TitleMaxLength)
-                .UniqueInsideOfDbSetColumn(context.Category, c => c.Title);
+                .UniqueInsideOfDbSetColumn(context.Category, c => c.Title)
+                .WhenAsync(CategoryTitleWasUpdated);
         }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Checks if the category title was changed in the current update.
+        /// </summary>
+        /// <param name="command">
+        /// Contains information about the current update of the category.
+        /// </param>
+        /// <param name="token"></param>
+        /// <returns></returns>
+        private async Task<bool> CategoryTitleWasUpdated(UpdateCategoryCommand command, CancellationToken token)
+        {
+            string currentCategoryTitle = await _context.Category
+                .Where(c => c.CategoryId == command.CategoryId)
+                .Select(c => c.Title)
+                .SingleOrDefaultAsync(token);
+            return currentCategoryTitle != command.Title;
+        } 
+        
+        #endregion
     }
 }
--- src/Application.Validation/AbstractValidators/CQRS/Posts/Commands/PostStorage/UpdatePostCommandValidator.cs
+++ src/Application.Validation/AbstractValidators/CQRS/Posts/Commands/PostStorage/UpdatePostCommandValidator.cs
@@ -1,18 +1,32 @@
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 using Application.CQRS.Posts.Commands.PostStorage;
 using Application.Persistence.Interfaces;
 using Application.Validation.Options;
 using Application.Validation.Tools.Extensions;
 using FluentValidation;
+using Microsoft.EntityFrameworkCore;
 
 namespace Application.Validation.AbstractValidators.CQRS.Posts.Commands.PostStorage
 {
     public class UpdatePostCommandValidator : AbstractValidator<UpdatePostCommand>
     {
+        #region Fields
+
+        private readonly IXNewsDbContext _context;
+
+        #endregion
+
+        #region Constructors
+
         public UpdatePostCommandValidator(IXNewsDbContext context)
         {
+            _context = context;
+
             RuleFor(c => c.PostId)
                 .NotEmpty();
-            
+
             RuleFor(c => c.Content)
                 .NotEmpty()
                 .Length(PostValidationOptions.ContentMinLength, PostValidationOptions.ContentMaxLength);
@@ -20,10 +34,34 @@ namespace Application.Validation.AbstractValidators.CQRS.Posts.Commands.PostStor
             RuleFor(c => c.Title)
                 .NotEmpty()
                 .Length(PostValidationOptions.TitleMinLength, PostValidationOptions.TitleMaxLength)
-                .UniqueInsideOfDbSetColumn(context.Post, p => p.Title);
+                .UniqueInsideOfDbSetColumn(context.Post, p => p.Title)
+                .WhenAsync(PostTitleWasUpdated);
 
             RuleFor(c => c.UserId)
                 .NotEmpty();
         }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Checks if the post title was changed in the current update.
+        /// </summary>
+        /// <param name="command">
+        /// Contains information about the current update of the post.
+        /// </param>
+        /// <param name="token"></param>
+        /// <returns></returns>
+        private async Task<bool> PostTitleWasUpdated(UpdatePostCommand command, CancellationToken token)
+        {
+            string currentPostTitle = await _context.Post
+                .Where(p => p.PostId == command.PostId)
+                .Select(p => p.Title)
+                .SingleOrDefaultAsync(token);
+            return currentPostTitle != command.Title;
+        }
+
+        #endregion
     }
 }
